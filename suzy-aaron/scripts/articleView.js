bba3e13fa33e6857d914356139d00c2284e6f643
Suzanne Su <suhysu@gmail.com>
2018-09-06 10:43:32 -0700

    handle auth filter displays. need to do else

--- suzy-aaron/scripts/articleView.js
+++ suzy-aaron/scripts/articleView.js
@@ -14,7 +14,7 @@ articleView.populateFilters = function() {
       authorName = $(this).attr('data-author');
 
       // TODO: Refactor this concatenation using a template literal.
-      optionTag = `<option value=" ${authorName} "> ${authorName} </option>`;
+      optionTag = `<option value="${authorName}"> ${authorName} </option>`;
 
       if ($('#author-filter option[value="' + authorName + '"]').length === 0) {
         $('#author-filter').append(optionTag);
@@ -40,10 +40,13 @@ articleView.handleAuthorFilter = function() {
     if ($(this).val()) {
       // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
+      $('article').hide();
+      let $selection = $(this).val();
+      $('article[data-author="' + $selection + '"]').fadeIn('slow');
 
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
-
+      $('article').show();
     }
     $('#category-filter').val('');
   });
@@ -77,4 +80,5 @@ articleView.setTeasers = function() {
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
