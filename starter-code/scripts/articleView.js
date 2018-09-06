02163d1d02cea038e52e4093dc65c26344c86a73
brinel321 <brian.c.nelson1@outlook.com>
2018-09-06 11:00:46 -0700

    finished tab feature

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -72,10 +72,13 @@ articleView.handleCategoryFilter = function() {
 
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to nav elements that will power the Tabs feature.
+  // TODO: [DONE] Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-
+  $(`.tab`).on('click', function(){
+    $(`.tab-content`).hide();
+    $('#' + $(this).data('content')).fadeIn();
+  })
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
   $('nav .tab:first').click();
 };
@@ -93,4 +96,5 @@ $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
+  articleView.handleMainNav();
 })
