1ae46b9a1b94e0c26319497807622805e9efc229
ellisken <ellisken@oregonstate.edu>
2018-09-06 11:36:58 -0700

    finish setTeasers()

--- andrew-kendra/scripts/articleView.js
+++ andrew-kendra/scripts/articleView.js
@@ -73,7 +73,6 @@ articleView.handleMainNav = function() {
 
   $('.tab').on('click', function() {
     let tabValue = $(this).data('content');
-    console.log(tabValue);
     $('.tab-content').hide();
     $(`section.tab-content[id="${tabValue}"]`).fadeIn();
   });
@@ -86,15 +85,23 @@ articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
 
-  // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  // DONE: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+  // Get #articles section and attach event handler to it to reveal all
+  $('#articles').on('click', 'a.read-on', function(e){
+    e.preventDefault();
+    $(this).hide();
+    //Show rest of article
+    console.log($(this).parent().children());
+    $(this).parent().children('.article-body').children().show();
+  });
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
+// DONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
-
+  articleView.setTeasers();
 })
