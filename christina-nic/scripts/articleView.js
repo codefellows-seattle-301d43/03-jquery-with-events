18629e113a2dc601089c169189e8a73058c6532c
Christina Gislason <mkcgislason@gmail.com>
2018-09-06 21:24:47 -0700

    handleMainNav and setTeasers functions working

--- christina-nic/scripts/articleView.js
+++ christina-nic/scripts/articleView.js
@@ -77,34 +77,46 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
-  $('.tab').on('click', event =>{
+  $('nav').on('click', '.tab', function(event) { 
+    event.preventDefault();
     $('.tab-content').hide();
-    $('#'+ $(this).data('content')).fadeIn();
-    console.log($(this).data('content'));
+    $('#' + $(this).data('content')).fadeIn();
   });
-
   
-
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
-  //$('nav .tab:first').click();
+  $('nav .tab:first').click();
 };
 
 articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
 
-  // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  // DONE: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
-};
+  $('article').on('click', 'a.read-on', function(event) {
+    event.preventDefault();
+    if ($(this).text() === 'Read on →') {
+      $(this).parent().find('*').fadeIn(1000);
+      $(this).html('Show less &larr;');
+    } else {
+      $('body').animate({
+        scrollTop: ($(this).parent().offset().top)
+      }, 200);  
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+    }
+  })
+}
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
+// DONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 })
